5d5288b97b0e29ea23d885da98cde8795ae08fd7
your name here <roelliga@yahoo.com>
2022-07-26 15:28:06 -0700

    doctors seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,14 @@
 # - Each doctor should have 10 patients with unique names
 # - Each patient should have 10 appointments (5 in the past, 5 in the future)
 #   - Each appointment should be 50 minutes in duration
+
+Doctor.create(name: "George")
+Doctor.create(name: "Harry")
+Doctor.create(name: "Bob")
+Doctor.create(name: "Jack")
+Doctor.create(name: "Ron")
+Doctor.create(name: "Jill")
+Doctor.create(name: "Kate")
+Doctor.create(name: "Kelly")
+Doctor.create(name: "Marge")
+Doctor.create(name: "Lisa")
