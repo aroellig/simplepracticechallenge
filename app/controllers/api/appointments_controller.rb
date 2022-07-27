834c30a97af6b7f6fd886be111da11492bdb0b5d
your name here <roelliga@yahoo.com>
2022-07-27 14:32:52 -0700

    returns all apointments

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -2,7 +2,37 @@ class Api::AppointmentsController < ApplicationController
   def index
     # TODO: return all values
     appointments = Appointment.all
-    render json: {data: appointments}
+    doctors = Doctor.all
+    patients = Patient.all
+    appointmentList = [];
+
+    appointments.each do |appointment|
+          appointmentObj = {
+      id: '',
+      patient:  '' ,
+      doctor:  { name: '', id: '' },
+      created_at: '',
+      start_time: '',
+      duration_in_minutes: ''
+    }
+      appointmentObj["id"] = appointment.id
+      appointmentObj["created_at"] = appointment.created_at
+      appointmentObj["start_time"] = appointment.start_time
+      appointmentObj["duration_in_minutes"] = appointment.duration_in_minutes
+        doctors.each do |doctor|
+          if doctor.id == appointment.doctor_id
+            appointmentObj["doctor"] = { name: doctor.name, id: doctor.id }
+          end
+        end
+
+        patients.each do |patient|
+          if patient.id == appointment.patient_id
+            appointmentObj["patient"] = {name: patient.name}
+          end
+        end
+      appointmentList.push(appointmentObj)
+    end
+    render json: {data: appointmentList}
     # TODO: return filtered values
     # head :ok
   
