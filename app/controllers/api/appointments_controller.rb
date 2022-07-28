db113bed3e37ec30f5087c53fceb24984071c11a
your name here <roelliga@yahoo.com>
2022-07-28 15:44:52 -0700

    seeds work

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -94,7 +94,38 @@ class Api::AppointmentsController < ApplicationController
           end
         appointmentList.push(appointmentObj)
         end 
+   
+
+  else
+
+    appointments.each do |appointment| 
+        
+      appointmentObj = {
+  id: '',
+  patient:  '' ,
+  doctor:  { name: '', id: '' },
+  created_at: '',
+  start_time: '',
+  duration_in_minutes: ''
+}
+  appointmentObj["id"] = appointment.id
+  appointmentObj["created_at"] = appointment.created_at
+  appointmentObj["start_time"] = appointment.start_time
+  appointmentObj["duration_in_minutes"] = appointment.duration_in_minutes
+    doctors.each do |doctor|
+      if doctor.id == appointment.doctor_id
+        appointmentObj["doctor"] = { name: doctor.name, id: doctor.id }
+      end
     end
+
+    patients.each do |patient|
+      if patient.id == appointment.patient_id
+        appointmentObj["patient"] = {name: patient.name}
+      end
+    end
+  appointmentList.push(appointmentObj)
+  end 
+end
 
      render json: {data: appointmentList}
 
